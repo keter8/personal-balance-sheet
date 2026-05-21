00ed15bd4fc89456a0d396105f123f94c8687eaa
keter8 <keterwang1206@gmail.com>
2026-05-21 23:03:53 +0800

    Label backup import export actions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "finance-ledger-v20";
-const ASSETS = ["./", "./index.html", "./styles.css?v=20", "./app.js?v=20", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE_NAME = "finance-ledger-v21";
+const ASSETS = ["./", "./index.html", "./styles.css?v=21", "./app.js?v=21", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
