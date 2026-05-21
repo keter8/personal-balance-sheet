852bd75104186dfc0b2ae7f5417003e20b6fcba3
keter8 <keterwang1206@gmail.com>
2026-05-21 23:07:10 +0800

    Refine backup action icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "finance-ledger-v21";
-const ASSETS = ["./", "./index.html", "./styles.css?v=21", "./app.js?v=21", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE_NAME = "finance-ledger-v22";
+const ASSETS = ["./", "./index.html", "./styles.css?v=22", "./app.js?v=22", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
