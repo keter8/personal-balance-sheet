2b60816600e237d617e6aa5d15a6c3a8c888ca5f
keter8 <keterwang1206@gmail.com>
2026-05-23 10:09:38 +0800

    Tidy property estimate form layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "finance-ledger-v54";
-const ASSETS = ["./", "./index.html", "./styles.css?v=54", "./app.js?v=54", "./manifest.webmanifest", "./icons/icon.svg"];
+const CACHE_NAME = "finance-ledger-v55";
+const ASSETS = ["./", "./index.html", "./styles.css?v=55", "./app.js?v=55", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
